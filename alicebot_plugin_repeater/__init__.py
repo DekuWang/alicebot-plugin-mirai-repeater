f6e07699c2ad9adf87f667c6ca0b287a07996ba0
DekuWang <2873450572@qq.com>
2024-07-30 16:26:18 +0800

    Remove Config.py

--- alicebot_plugin_repeater/__init__.py
+++ alicebot_plugin_repeater/__init__.py
@@ -1,53 +1,50 @@
 # For repeating group member's message
 
 # First party modules
-from typing import Dict, List, Optional
+from random import randint
 # Third party modules
-from alicebot import Plugin, ConfigModel
-from alicebot.adapter.mirai.event import MessageEvent
-# Project Module
-from Config import Config
+from alicebot import Plugin
 
-
-class Repeater(Plugin[MessageEvent, int, Config], config = Config):
+class Repeater(Plugin):
     priority = 2
-    block = False
-    
+    block = 0
+    repeat_len = 2
+    message_record = {}
+
     async def handle(self) -> None:
-        try:                                                        #TODO: support other message types, such as meme or image
+        try:                                                    #TODO: Support other message types
             await self.event.reply(self.event.messageChain)
         except:
             pass
     
-    async def rule(self) -> bool:
-            if (self.event.adapter.name == "mirai" and 
-                self.event.type == "GroupMessage"):
+    async def rule(self):
+        if (self.event.adapter.name == "mirai" and 
+            self.event.type == "GroupMessage"):
 
-                current_message = str(self.event.messageChain)
-                current_group: int = self.event.sender.group.id                                                  
+            current_message = str(self.event.messageChain)
+            current_group = self.event.sender.group.id
 
-                if not self.config.message_record.get(current_group):                                            
-                    self.config.message_record[current_group] = [current_message]
-                    return False
-                
-                if len(self.config.message_record.get(current_group)) < self.config.repeat_len:
-                    if current_message == self.config.message_record.get(current_group)[-1]:
-                        self.config.message_record[current_group].append(current_message)
-                        if len(self.config.message_record.get(current_group)) == self.config.repeat_len:
-                            return True
-                    else:
-                        self.config.message_record[current_group].clear()
-                        self.config.message_record[current_group].append(current_message)
-                    return False
+            if not self.message_record.get(current_group):
+                self.message_record[current_group] = [current_message]
+                return
+            
+            if len(self.message_record.get(current_group)) < self.repeat_len:
+                if current_message == self.message_record.get(current_group)[-1]:
+                    self.message_record[current_group].append(current_message)
+                    if len(self.message_record.get(current_group)) == self.repeat_len:
+                        return True
                 else:
-                    if current_message == self.config.message_record.get(current_group)[-1]:
-                        pass
-                    else:
-                        self.config.message_record[current_group].clear()
-                        self.config.message_record[current_group].append(current_message)
-
-                    return False
+                    self.message_record[current_group].clear()
+                    self.message_record[current_group].append(current_message)
             else:
-                return False
+                if current_message == self.message_record.get(current_group)[-1]:
+                    pass
+                else:
+                    self.message_record[current_group].clear()
+                    self.repeat_len = randint(1,4)
+                    print(self.repeat_len)
+                    self.message_record[current_group].append(current_message)
+        else:
+            return False
 
 
